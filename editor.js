381998cba524cb944aa98df8bc93e4ee241bfd95
Melissa.Kinsey <kinsey.melissa@gmail.com>
2022-07-31 09:25:16 -0500

    [feat] Add Material icons for cut copy, print, undo, redo, and clear formatting. Replace background image with a more saturated version. Change color of editable fields.

--- editor.js
+++ editor.js
@@ -1,3 +1,4 @@
+const { prepareDocs } = require("mongodb/lib/operations/common_functions");
 
                 var oDoc, sDefTxt;
 
@@ -7,17 +8,36 @@
                     sDefTxt = oDoc.innerHTML;
                     //if (document.compForm.switchMode.checked) { setDocMode(true); }
                 }
-
+                let copied = null
                 function formatDoc(sCmd, sValue)
                 {
-                    if (validateMode()) { document.execCommand(sCmd, false, sValue); oDoc.focus(); }
+                    //event.preventDefault()
+                    if (validateMode())
+                    {
+                       // oDoc.focus();
+                        if (sCmd === 'copy')
+                        {
+                            //https://stackoverflow.com/questions/6300213/copy-selected-text-to-the-clipboard-without-using-flash-must-be-cross-browser
+                            //https://developer.mozilla.org/en-US/docs/Web/API/Element/paste_event
+                            let selection = window.getSelection()
+                                copied = selection.
+                                anchorNode.data.slice(selection.anchorOffset,selection.focusOffset);
+                            console.log(copied);
+                        } else {
+                           // debugger
+                            let res = document.execCommand(sCmd, false, sValue);
+                        }
+                    }
                 }
 
                 function validateMode()
                 {
-                    if (!document.compForm.switchMode.checked) { return true; }
-                    alert("Uncheck \"Show HTML\".");
+                    if (!document.compForm.switchMode.checked)
+                    {
+                        return true;
+                    }
                     oDoc.focus();
+                    alert("Uncheck \"Show HTML\".");
                     return false;
                 }               
 
